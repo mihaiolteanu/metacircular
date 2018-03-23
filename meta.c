25c8800b9c41f8b77f2db3fd0ff2154a3d2c34ee
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2018-03-23 12:59:26 +0200

    Typedef pointers to objects/pairs/numbers/etc
    It seems more natural to use these as now everything is
    passed by reference, as would be the case in Lisp

--- meta.c
+++ meta.c
@@ -11,73 +11,80 @@ typedef enum {
 typedef struct {
     void *pointer;
     object_type type;
-} object;
+} object_;
+
+typedef object_ *object;
 
 typedef struct {
-    object *car;
-    object *cdr;
-} pair;
+    object car;
+    object cdr;
+} pair_;
+
+typedef pair_ *pair;
 
 typedef struct {
     int value;
-} number_object;
+} number_object_;
+
+typedef number_object_ *number_object;
 
 void *allocate(size_t size) {
     return malloc(size);
 }
 
-number_object *new_number(long long value) {
-    number_object *number = allocate(sizeof(number_object));
+number_object new_number(long long value) {
+    number_object_ *number = allocate(sizeof(number_object_));
     number->value = value;
     return number;
 }
 
-object *new_object(void *pointer, object_type type) {
-    object *o = allocate(sizeof(object));
+object new_object(void *pointer, object_type type) {
+    object_ *o = allocate(sizeof(object_));
     o->pointer = pointer;
     o->type = type;
+    return o;
 }
 
-pair *cons(object *car, object *cdr)
+pair cons(object car, object cdr)
 {
-    pair *p = allocate(sizeof(pair));
+    pair_ *p = allocate(sizeof(pair_));
     p->car = car;
     p->cdr = cdr;
     return p;
 }
 
-void print_pair(pair *p);
+void print_pair(pair p);
 
-object *car(pair *p) {
+object car(pair p) {
     return p->car;
 }
 
-object *cdr(pair *p) {
+object cdr(pair p) {
     return p->cdr;
 }
 
-object_type type(object *o) {
+object_type type(object o) {
     return o->type;
 }
 
-void print_number(number_object *number) {
+void print_number(number_object number) {
     printf("%d", number->value);
 }
 
-void print_object(object *o) {
+void print_object(object o) {
     switch(type(o)) {
     case number_type:
-        print_number((number_object *)o->pointer);
+        print_number((number_object)o->pointer);
         break;
     case pair_type:
-        print_pair((pair *)o->pointer);
+        print_pair((pair)o->pointer);
         break;
     default:
         break;
     }
 }
 
-void print_pair(pair *p) {
+void print_pair(pair p) {
     printf("(");
     print_object(car(p));
     printf(" ");
@@ -85,21 +92,21 @@ void print_pair(pair *p) {
     printf(")");
 }
 
-void print_pair_main(pair *p) {
+void print_pair_main(pair p) {
     print_pair(p);
     printf("\n");
 }
 
 int main(void) {
-    number_object *number5  = new_number( 5);
-    number_object *number10 = new_number(10);
-    number_object *number15 = new_number(15);
+    number_object number5  = new_number( 5);
+    number_object number10 = new_number(10);
+    number_object number15 = new_number(15);
     
-    pair *p1 = cons(new_object(number5, number_type),
-                    new_object(number10, number_type));
-
-    pair *p2 = cons(new_object(number15, number_type),
-                    new_object(p1, pair_type));
+    pair p1 = cons(new_object(number5, number_type),
+                   new_object(number10, number_type));
+    
+    pair p2 = cons(new_object(number15, number_type),
+                   new_object(p1, pair_type));
     print_pair_main(p2);
     
     return 0;
