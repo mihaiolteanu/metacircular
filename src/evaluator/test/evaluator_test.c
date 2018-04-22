d81787521f119acebaf4f1c1cf4f85e0788c21c4
mihaiolteanu <mihai_olteanu@fastmail.fm>
2018-04-22 14:26:48 +0300

    Add test for definition evaluation

--- src/evaluator/test/evaluator_test.c
+++ src/evaluator/test/evaluator_test.c
@@ -1,16 +1,16 @@
 #include "unity.h"
 #include "evaluator.h"
 
-/* void test_eval_definition() { */
-/*     /\* construct and eval (define speed 100) *\/ */
-/*     object def = new_identifier("define"); */
-/*     object speed = new_identifier("speed"); */
-/*     object kmph = new_number(100); */
-/*     object def_expr = cons(def, cons(speed, cons(kmph, nil))); */
-/*     environment env = extend_environment(null_environment); */
-/*     eval(def_expr, env); */
+void test_eval_definition() {
+    object def = new_identifier("define");
+    object speed = new_identifier("speed");
+    object kmph = new_number(100);
+    /* '(define speed 100) */
+    object def_expr = cons(def, cons(speed, cons(kmph, nil)));
+    environment env = extend_environment(null_environment);
+    eval(def_expr, env);
 
-/*     symbol s = find_symbol("speed", env); */
-/*     TEST_ASSERT_NOT_NULL(s); */
-/*     TEST_ASSERT_EQUAL(kmph, symbol_object(s)); */
-/* } */
+    object o = find("speed", env);
+    TEST_ASSERT_NOT_NULL(o);
+    TEST_ASSERT_EQUAL(100, number_value(o));
+}
