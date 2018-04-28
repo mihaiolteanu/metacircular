83350a5dc72653fe219b5ed53d54115d07d3866a
mihaiolteanu <mihai_olteanu@fastmail.fm>
2018-04-28 17:45:48 +0300

    Refactor/simplify the input tests

--- src/input/test/input_test.c
+++ src/input/test/input_test.c
@@ -2,92 +2,94 @@
 #include "base.h"
 #include "input.h"
 
+void number_test(object o, int expected) {
+    TEST_ASSERT_TRUE(is_number(o));
+    TEST_ASSERT_EQUAL(expected, number_value(o));
+}
+
+void identifier_test(object o, char *expected) {
+    TEST_ASSERT_TRUE(is_identifier(o));
+    TEST_ASSERT_EQUAL_STRING(expected, identifier_name(o));
+}
+
+void null_object_test(object o) {
+    TEST_ASSERT_TRUE(null_object(o));
+}
+
+void not_null_object_test(object o) {
+    TEST_ASSERT_FALSE(null_object(o));
+}
+
 void test_parse_number(void) {
     object o = parse("5");
-    TEST_ASSERT_TRUE(is_number(o));
-    TEST_ASSERT_EQUAL(5, number_value(o));
+    number_test(o, 5);
 }
 
 void test_parse_single_identifier(void) {
     object o = parse("(myf)");
-    TEST_ASSERT_NOT_NULL(o);
+    not_null_object_test(o);
     object car_o = car(o);
     object cdr_o = cdr(o);
-    TEST_ASSERT_TRUE(is_identifier(car_o));
-    TEST_ASSERT_EQUAL_STRING("myf", identifier_name(car_o));
-    TEST_ASSERT_TRUE(null_object(cdr_o));
+    identifier_test(car_o, "myf");
+    null_object_test(cdr_o);
 }
 
 void test_parse_multiple_identifier_list(void) {
     object o = parse("(myf 5 10)");
-    TEST_ASSERT_NOT_NULL(o);
+    not_null_object_test(o);
     object car_o = car(o);
-    TEST_ASSERT_TRUE(is_identifier(car_o));
-    TEST_ASSERT_EQUAL_STRING("myf", identifier_name(car_o));
-    object cadr_o = car(cdr(o));
-    TEST_ASSERT_TRUE(is_number(cadr_o));
-    TEST_ASSERT_EQUAL(5, number_value(cadr_o));
-    object caddr_o = car(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(is_number(caddr_o));
-    TEST_ASSERT_EQUAL(10, number_value(caddr_o));
-    object cdddr_o = cdr(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(null_object(cdddr_o));
+    identifier_test(car_o, "myf");
+    object cadr = car(cdr(o));
+    number_test(cadr, 5);
+    object caddr = car(cdr(cdr(o)));
+    number_test(caddr, 10);
+    object cdddr = cdr(cdr(cdr(o)));
+    null_object_test(cdddr);
 }
 
 void test_parse_embedded_list(void) {
     object o = parse("((myf) 5 10)");
-    TEST_ASSERT_NOT_NULL(o);
+    not_null_object_test(o);
     object car_o = car(o);
-    object car_car_o = car(car_o);
-    TEST_ASSERT_TRUE(is_identifier(car_car_o));
-    TEST_ASSERT_EQUAL_STRING("myf", identifier_name(car_car_o));
-    object cadr_o = car(cdr(o));
-    TEST_ASSERT_TRUE(is_number(cadr_o));
-    TEST_ASSERT_EQUAL(5, number_value(cadr_o));
-    object caddr_o = car(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(is_number(caddr_o));
-    TEST_ASSERT_EQUAL(10, number_value(caddr_o));
-    object cdddr_o = cdr(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(null_object(cdddr_o));
+    object caar = car(car_o);
+    identifier_test(caar, "myf");
+    object cadr = car(cdr(o));
+    number_test(cadr, 5);
+    object caddr = car(cdr(cdr(o)));
+    number_test(caddr, 10);
+    object cdddr = cdr(cdr(cdr(o)));
+    null_object_test(cdddr);
 }
 
 void test_parse_embedded_middle_list(void) {
     object o = parse("(5 (myf) 10)");
-    TEST_ASSERT_NOT_NULL(o);
+    not_null_object_test(o);
     object car_o = car(o);
-    TEST_ASSERT_TRUE(is_number(car_o));
-    TEST_ASSERT_EQUAL(5, number_value(car_o));
+    number_test(car_o, 5);
     object caadr = car(car(cdr(o)));
-    TEST_ASSERT_TRUE(is_identifier(caadr));
-    TEST_ASSERT_EQUAL_STRING("myf", identifier_name(caadr));
+    identifier_test(caadr, "myf");
     object caddr = car(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(is_number(caddr));
-    TEST_ASSERT_EQUAL(10, number_value(caddr));
+    number_test(caddr, 10);
     object cdddr = cdr(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(null_object(cdddr));
+    null_object_test(cdddr);
 }
 
 void test_parse_multiple_embedded_lists(void) {
     object o = parse("(lambda (x) (+ x 1))");
-    TEST_ASSERT_NOT_NULL(o);
+    not_null_object_test(o);
     object car_o = car(o);
-    TEST_ASSERT_TRUE(is_identifier(car_o));
-    TEST_ASSERT_EQUAL_STRING("lambda", identifier_name(car_o));
+    identifier_test(car_o, "lambda");
     object caadr = car(car(cdr(o)));
-    TEST_ASSERT_TRUE(is_identifier(caadr));
-    TEST_ASSERT_EQUAL_STRING("x", identifier_name(caadr));
+    identifier_test(caadr, "x");
     object cddr = cdr(cdr(o));
     object caaddr = car(car(cddr));
-    TEST_ASSERT_TRUE(is_identifier(caaddr));
-    TEST_ASSERT_EQUAL_STRING("+", identifier_name(caaddr));
+    identifier_test(caaddr, "+");
     object cadaddr = car(cdr(car(cdr(cdr(o)))));
-    TEST_ASSERT_TRUE(is_identifier(cadaddr));
-    TEST_ASSERT_EQUAL_STRING("x", identifier_name(cadaddr));
+    identifier_test(cadaddr, "x");
     object caddaddr = car(cdr(cdr(car(cdr(cdr(o))))));
-    TEST_ASSERT_TRUE(is_number(caddaddr));
-    TEST_ASSERT_EQUAL(1, number_value(caddaddr));
+    number_test(caddaddr, 1);
     object cdddaddr = cdr(cdr(cdr(car(cdr(cdr(o))))));
-    TEST_ASSERT_TRUE(null_object(cdddaddr));
+    null_object_test(cdddaddr);
     object cdddr = cdr(cdr(cdr(o)));
-    TEST_ASSERT_TRUE(null_object(cdddr));
+    null_object_test(cdddr);
 }
